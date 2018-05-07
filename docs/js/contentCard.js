8725e9a32d325145e003e57c9908b7ab9300403e
Ricardo E. Galvez <ricardogalvez@nypl.org>
2018-05-07 07:49:34 -0400

    :meat_on_bone: now with some logic to make sure we dont render html that does not have an ebook url

--- docs/js/contentCard.js
+++ docs/js/contentCard.js
@@ -17,20 +17,22 @@ function loadJSON(callback) {
 
 // Call to function with anonymous callback
 loadJSON(response => {
-  // Do Something with the response e.g.
+  // Do Something with the response
   const staffPicksJSON = JSON.parse(response);
   //console.log(staffPicksJSON.picks);
   
   staffPicksJSON.picks.forEach(pick => {
-    const cardMarkUp = `
-    <li class="book-item love-stories seriously-good-writing ">
-      <div class="book-item-image-box">
-        <img alt="" src="${pick.book.imageUrl}">
-      </div>
-    <h3 class="book-item-title">${pick.book.title}</h3>
-    <p class="book-item-author">${pick.book.author}</p>
-    <div class="book-item-catalog-links">
-      <a href="${pick.book.catalogUrl}" class="catalog-url" aria-label="Request Book: ${pick.book.title}">
+    //console.log(pick.book.catalogUrl);
+    const ebookURL = pick.book.ebookUrl;
+    const ebookTitle = pick.book.title;
+    //const catalogURL = pick.book.catalogUrl;
+    
+    function checkForURL(){
+      
+      if (ebookURL != undefined) {
+        //console.log('this ebook has a url: \n' + ebookURL);
+        const itemUrlMarkup = `
+        <a href="${pick.book.catalogUrl}" class="catalog-url" aria-label="Request Book: ${pick.book.title}">
         <svg width="32px" height="32px" viewBox="0 0 32 32" class="book-icon svgIcon" aria-hidden="true" preserveAspectRatio="xMidYMid meet">
           <title>NYPL Book SVG Icon</title>
           <g>
@@ -52,6 +54,38 @@ loadJSON(response => {
         </svg>
         <span>Request E-Book</span>
       </a>
+        `;
+        return itemUrlMarkup;
+      } else {
+        //console.log('this pick does not have an ebook available: \n' + ebookTitle);
+        const itemUrlMarkup = `
+        <a href="${pick.book.catalogUrl}" class="catalog-url" aria-label="Request Book: ${pick.book.title}">
+        <svg width="32px" height="32px" viewBox="0 0 32 32" class="book-icon svgIcon" aria-hidden="true" preserveAspectRatio="xMidYMid meet">
+          <title>NYPL Book SVG Icon</title>
+          <g>
+            <path d="M26.09478,8.31652A13.1617,13.1617,0,0,0,15.97189,8.1603a13.079,13.079,0,0,0-10.09165.18746,0.88881,0.88881,0,0,0-.46865.84357V21.81371a0.89164,0.89164,0,0,0,.37492.81233,0.93487,0.93487,0,0,0,.90606.06249,11.44018,11.44018,0,0,1,8.9044,0c0.03124,0,.03124,0,0.03124.03124a0.06237,0.06237,0,0,1,.06249.03124,0.70873,0.70873,0,0,0,.125.03124h0.18746a0.5317,0.5317,0,0,0,.2187-0.03124,0.06237,0.06237,0,0,0,.06249-0.03124,0.70873,0.70873,0,0,1,.125-0.03124,11.464,11.464,0,0,1,8.9044,0,0.86486,0.86486,0,0,0,.40617.09373A1.05326,1.05326,0,0,0,26.251,22.626a0.94244,0.94244,0,0,0,.43741-0.81233V9.19133A1.04849,1.04849,0,0,0,26.09478,8.31652ZM24.78256,20.439a13.30172,13.30172,0,0,0-8.74818.34368A13.26349,13.26349,0,0,0,7.2862,20.439V9.8162a11.30538,11.30538,0,0,1,8.342.2187c0.03124,0,.03124,0,0.03124.03124a0.06237,0.06237,0,0,1,.06249.03124,0.70872,0.70872,0,0,0,.125.03124h0.34368a0.11478,0.11478,0,0,0,.09373-0.03124c0.03125,0,.06249-0.03124.09373-0.03124,0,0,.06249,0,0.06249-0.03124a11.51979,11.51979,0,0,1,8.37326-.2187V20.439H24.78256Z"></path>
+            <path d="M16.00313,17.81454a0.76028,0.76028,0,0,1-.74984-0.74984V13.378a0.74984,0.74984,0,0,1,1.49969,0V17.0647A0.74014,0.74014,0,0,1,16.00313,17.81454Z"></path>
+          </g>
+        </svg>
+        <span>Request Book</span>
+      </a>
+      `;
+      return itemUrlMarkup;
+      }
+    }
+    const insertURL = checkForURL();
+    //console.log(insertURL);
+    
+    const cardMarkUp = `
+    <li class="book-item love-stories seriously-good-writing ">
+      <div class="book-item-image-box">
+        <img alt="" src="${pick.book.imageUrl}">
+      </div>
+    <h3 class="book-item-title">${pick.book.title}</h3>
+    <p class="book-item-author">${pick.book.author}</p>
+    <div class="book-item-catalog-links">
+      <!-- the url block -->
+      ${insertURL}
     </div>
     <div>
       <p class="book-item-description">${pick.reviews[0].text}</p>
